c26d73ce92377c13762a3d9459f2c2b64b97f0df
RohitKushvaha01 <oldisg131@gmail.com>
2024-11-02 15:29:51 +0530

    feat. main activity

--- app/src/main/java/com/rk/xededitor/ui/activities/mainactivity/MainActivity.kt
+++ app/src/main/java/com/rk/xededitor/ui/activities/mainactivity/MainActivity.kt
@@ -1,19 +1,67 @@
 package com.rk.xededitor.ui.activities.mainactivity
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.rk.xededitor.BaseActivity
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.rk.xededitor.ui.theme.KarbonTheme
+import kotlinx.coroutines.launch
 
-class MainActivity : BaseActivity() {
+class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             KarbonTheme {
-            
+                MainScreen()
             }
+            
         }
+    }
+    
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun MainScreen() {
+        val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+        val scope = rememberCoroutineScope()
+        
+        
+        ModalNavigationDrawer(drawerState = drawerState, drawerContent = {
+            ModalDrawerSheet {
+                //drawer content
+            }
+        }, content = {
+            Scaffold(topBar = {
+                TopAppBar(title = { }, navigationIcon = {
+                    IconButton(onClick = {
+                        scope.launch {
+                            drawerState.open()
+                        }
+                    }) {
+                        Icon(Icons.Default.Menu, "Menu")
+                    }
+                })
+            }) { paddingValues ->
+                //content
+                Text(text = "Content", modifier = Modifier.padding(paddingValues))
+            }
+        })
     }
 }
