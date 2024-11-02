03c9266bdc31f7d01046bdc6eac5e8f87a262c85
RohitKushvaha01 <oldisg131@gmail.com>
2024-11-02 20:24:08 +0530

    feat. viewmodel

--- app/src/main/java/com/rk/xededitor/ui/activities/mainactivity/MainActivity.kt
+++ app/src/main/java/com/rk/xededitor/ui/activities/mainactivity/MainActivity.kt
@@ -4,35 +4,70 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material3.*
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.rk.xededitor.MainActivity.file.getFragmentType
+import com.rk.xededitor.MainActivity.tabs.core.FragmentType
+import com.rk.xededitor.ui.components.ScrollableTabLayout
 import com.rk.xededitor.ui.theme.KarbonTheme
 import kotlinx.coroutines.launch
+import java.io.File
 
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalMaterial3Api::class)
+    lateinit var viewModel: MyViewModel
+    
+    class MyViewModel : ViewModel() {
+        val _openedTabs = mutableListOf<String>()
+        val _openedTabsTypes = mutableListOf<FragmentType>()
+        val _openedTabsFiles = mutableListOf<File?>()
+        
+        fun newTab(title:String,type: FragmentType) {
+            _openedTabs.add(title)
+            _openedTabsTypes.add(type)
+            _openedTabsFiles.add(null)
+        }
+        
+        fun newTab(file: File,type: FragmentType? = null) {
+            _openedTabs.add(file.name)
+            _openedTabsTypes.add(type ?: file.getFragmentType())
+            _openedTabsFiles.add(file)
+        }
+        
+        fun removeAt(index:Int){
+            _openedTabs.removeAt(index)
+            _openedTabsTypes.removeAt(index)
+            _openedTabsFiles.removeAt(index)
+        }
+        
+        
+    }
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            viewModel = viewModel()
             KarbonTheme {
                 MainScreen()
             }
-            
         }
     }
     
@@ -59,8 +94,20 @@ class MainActivity : ComponentActivity() {
                     }
                 })
             }) { paddingValues ->
-                //content
-                Text(text = "Content", modifier = Modifier.padding(paddingValues))
+                
+                ScrollableTabLayout(
+                    Modifier.padding(paddingValues),
+                    tabs = viewModel._openedTabs,
+                    content = {
+                        Box(Modifier.fillMaxSize()) {
+                            viewModel._openedTabsFiles[it]?.let { it1 -> Text(it1.absolutePath) }
+                        }
+                    },
+                    animation = false
+                )
+                
+                
+                
             }
         })
     }
